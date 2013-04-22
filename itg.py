2bdce241be4970ccde4c544d1d83a2d1ad0c8de4
ohnorobo <konyater@gmail.com>
2013-04-22 00:15:10 -0400

    initial commit

---
+++ itg.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+
+ENGLISH_FILE = "test.en"
+GERMAN_FILE = "test.de"
+PROBS_FILE = "itg.dict"
+probs = {}
+
+#log probabilities
+AB = -1   # A -> [AA]
+BA = -2   # A -> ⟨AA⟩
+WE = -20  # A -> word.en / ϵ
+EW = -21  # A -> ϵ / word.de
+
+WW = -100 # A -> word.en / word.de
+          # if there is no entry for the pair in PROBS_FILE
+
+#import sys
+#sys.setrecursionlimit(20)
+
+
+
+#############Representation of Parse Trees
+
+class Parse():
+    pass
+
+class Node(Parse):
+
+    def __init__(self, left, right, prob, inverted):
+        """
+        Args:
+            left - Parse
+            right - Parse
+            prob - float
+            inverted -  boolean, is this parse inverted in the target?
+        """
+        self.left = left
+        self.right = right
+        self.prob = prob
+        self.inverted = inverted
+
+    def __str__(self):
+        if self.inverted:
+            return ' ⟨' + str(self.left) + ',' + str(self.right) + '⟩ '
+        else:
+            return ' [' + str(self.left) + ',' + str(self.right) + '] '
+
+    def get_alignments(self):
+        a = self.left.get_alignments()
+        a.extend(self.right.get_alignments())
+        return a
+
+    __repr__ = __str__
+
+class Leaf(Parse):
+
+    def __init__(self, en, de, prob):
+        """
+        Args:
+            en - string, english word
+            de - string, german word
+            prob - float
+        """
+        self.en = en
+        self.de = de
+        self.prob = prob
+
+    def __str__(self):
+        return str(self.en) + '/' + str(self.de)
+
+    def get_alignments(self):
+        return [(self.en, self.de)]
+
+    __repr__ = __str__
+
+###############
+
+
+
+def read_probs(filename):
+    global probs
+    p = file(filename, 'r')
+
+    for line in p:
+        english, german, prob = line.split()
+        #print english, german, float(prob)
+        probs[(english, german)] = float(prob)
+
+
+def align(english, german):
+    print english, german
+
+    en = english.split()
+    de = german.split()
+
+    parse = prob_align(en, 0, len(en), de, 0, len(de), 0)
+
+    print parse
+    print parse.prob
+    alignment = parse.get_alignments()
+    print " "
+    print_alignment(alignment)
+    print "==="
+
+def print_alignment(alignment):
+    for pair in alignment:
+        if isinstance(pair[0], list):
+            for a in pair[0]:
+                print a, pair[1]
+        elif isinstance(pair[1], list):
+            for a in pair[1]:
+                print pair[0], a
+        else:
+            print pair[0], pair[1]
+
+
+
+
+
+
+def prob_align(en, i, k, de, u, w, depth):
+    """
+    Args:
+        en - list of strings, english sentence
+        de - list of strings, german sentence
+        i, k - ints, english start and end index
+        u, w - ints, german start and end index
+
+    Return:
+        returns a Parse, wth the most probable alignment for these indicies
+    """
+
+    #print "aligning", en[i:k], " : ", de[u:w]
+    #print " "*depth, "a", i, k, ":", u, w
+
+
+    ##Base Cases
+    #word / ϵ
+    if i == k: # is w-u == 1 ?
+        word_de = de[u:w]
+        #print 'ϵ', word_de
+        return Leaf('ϵ', word_de, EW*len(word_de))
+
+    #ϵ / word
+    if u == w: # is k-1 == 1 ?
+        word_en = en[i:k]
+        #print word_en, 'ϵ'
+        return Leaf(word_en, 'ϵ', WE*len(word_en))
+
+    #word / word
+    if k-i == 1 and w-u == 1:
+        word_en = en[i:k][0]
+        word_de = de[u:w][0]
+        #print word_en, word_de
+        return Leaf(word_en, word_de, word_alignment_prob(word_en, word_de))
+
+    ##Recursive Case
+    else:
+        parses = []
+
+        for j in xrange(i, k):
+            for v in xrange(u, w):
+                if j==i and v==u:
+                    pass
+                elif j==k and v==w:
+                    pass
+                else:
+
+                    #aligned
+                    left = prob_align(en, i, j, de, u, v, depth+1)
+                    right = prob_align(en, j, k, de, v, w, depth+1)
+                    parse = Node(left,
+                                 right,
+                                 left.prob+right.prob+AB,
+                                 False)
+                    parses.append(parse)
+
+                    #inverted
+                    left_inv = prob_align(en, i, j, de, v, w, depth+1)
+                    right_inv = prob_align(en, j, k, de, u, v, depth+1)
+                    parse_inv = Node(left_inv,
+                                     right_inv,
+                                     left_inv.prob+right_inv.prob+BA,
+                                     True)
+                    parses.append(parse_inv)
+
+                #pprint( parses )
+
+        return max(parses, key=lambda x: x.prob)
+
+
+
+def word_alignment_prob(word_en, word_de):
+    global probs
+
+    pair = (word_en, word_de)
+    if pair in probs:
+        return probs[pair]
+    else:
+        return WW
+
+
+
+
+
+
+#####
+
+if __name__ == "__main__": #i.e. run directly
+    read_probs(PROBS_FILE)
+    en = file(ENGLISH_FILE, 'r')
+    de = file(GERMAN_FILE, 'r')
+
+    for en_sentence, de_sentence in zip(en, de):
+        align(en_sentence, de_sentence)
+
---
+++ test.en.cp
@@ -0,0 +1,102 @@
+what is that .
+what is the news .
+i have therefore voted in favour of the report .
+this is a significant step forward .
+i must , nevertheless , make some specific comments .
+they will need time for that .
+that is surely right .
+these are the three characteristics .
+we say : ' freedom for iraq !
+a free i-raq !
+but i wanted to focus on the middle east .
+but the resolution as a whole remains ambiguous .
+this is why i abstained on this text .
+iraq is rich in petroleum .
+saddam hussein is a cruel dictator .
+it could only make a delicate situation worse .
+we want to talk . we want to negotiate .
+this time we hope it will involve parliament .
+this offers a way forward for the future .
+he prefers legal plundering .
+and now , bon app?tit !
+that concludes the explanations of vote .
+i declare adjourned the session of the european parliament .
+( the sitting was closed at <num> p.m. )
+resumption of the session
+- ' enough is enough ' .
+? basta ya !
+are there any comments ?
+subject to that observation , the minutes are approved .
+the following change has been proposed :
+wednesday :
+( parliament agreed to the request )
+wonderful !
+we can do that , mr leinen .
+i hope he will be able to come .
+human damage cannot be repaired .
+it is impossible to evaluate human suffering .
+i do not therefore accept this kind of argument .
+this crime can be avoided .
+you said that those comments were not correct .
+no , i will not do that .
+what they put in as a quotation was correct .
+no , it is not a point for plenary .
+you may take it up with me privately .
+' )
+that concludes the item .
+thank you for coming .
+we need both .
+this synthesis is difficult and painful .
+we are drawing closer together .
+we are becoming ever more dependent on each other .
+when others are stricken , we are affected .
+we need europe to be stronger and more determined .
+we have to do our homework for ourselves .
+europe has a vital interest in this issue .
+human rights must always be nurtured and promoted .
+europe can make a difference .
+there is no need to shy away from this .
+open up your markets ? trade means work .
+we must respond .
+allow me to address a couple of issues .
+trade is important , of course .
+i shall cite three examples .
+we are part of the imf .
+the european union is the best example of this .
+there is certainly no way back .
+the sea represents life itself .
+the theory is now belied by the facts .
+i believe that the response could be affirmative .
+i shall not dwell on agricultural issues .
+commissioner lamy has the floor once again .
+the union 's position remains the same .
+there must be no misunderstanding on this point .
+discussions have begun again .
+we must make progress .
+that is why we must seek innovative solutions .
+this is an incredibly important issue .
+neither is an adequate solution .
+this is inadequate !
+hence the regulation by the council of ministers .
+let us welcome your proposal of <num> january .
+i wish you very good luck , commissioner .
+unilateralism with a reason is just about comprehensible .
+unilateralism without any logic whatsoever is unforgivable .
+it has still not been solved .
+ultimately the political breakthrough came .
+that applies to all developing countries and all medicines .
+so : this far and no further .
+necessity knows no law .
+the trips agreements must not be a limiting factor .
+then there will be no development round !
+that is why we are taking this clear stance .
+then new medicines will have to be developed .
+that is my first point .
+this will undercut the profits of legitimate pharmaceutical industries .
+is that something you see as likely , commissioner ?
+that was not the intention at doha .
+what do we do then ?
+this provisional arrangement is simply not enough however .
+a long-term solution must therefore be found .
+something else enters into this , however .
+and this is not always the case .
